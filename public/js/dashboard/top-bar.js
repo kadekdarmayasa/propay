de5ae70ef0d9762f8d0d1c393c89c36885093863
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-02-28 20:21:31 +0800

    Adding feature when user click mode toggle button the icon of the profile toggle is changed

--- public/js/dashboard/top-bar.js
+++ public/js/dashboard/top-bar.js
@@ -1,8 +1,28 @@
 const profileToggle = document.querySelector('.profile-toggle');
 const profileContent = document.querySelector('.notification-profile .profile');
+const modeToggleBtn = document.querySelector('.mode-toggle');
+
+modeToggleBtn.addEventListener('click', function () {
+	const switcher = window.getComputedStyle(document.querySelector('.mode-toggle .switch'), '::before');
+
+	if (switcher.getPropertyValue('left') == '20px') {
+		if (profileContent.classList.contains('show')) {
+			profileToggle.firstElementChild.style.stroke = 'var(--primary-color)';
+		} else {
+			profileToggle.firstElementChild.style.stroke = 'var(--black-color)';
+		}
+	} else {
+		if (profileContent.classList.contains('show')) {
+			profileToggle.firstElementChild.style.stroke = 'var(--primary-color)';
+		} else {
+			profileToggle.firstElementChild.style.stroke = 'var(--white-color)';
+		}
+	}
+});
 
 profileToggle.addEventListener('click', function () {
 	profileContent.classList.toggle('show');
+
 	if (profileContent.classList.contains('show')) {
 		this.firstElementChild.style.stroke = 'var(--primary-color)';
 	} else {

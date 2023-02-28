7c8da74393aecbe1ca822f3bec73d588e65a4273
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-02-28 19:33:21 +0800

    Adding click behavior on the profile view

--- public/js/dashboard/top-bar.js
+++ public/js/dashboard/top-bar.js
@@ -1,5 +1,15 @@
 const profileToggle = document.querySelector('.profile-toggle');
+const profileContent = document.querySelector('.notification-profile .profile');
 
-profileToggle.addEventListener('click', () => {
-	alert('clicked');
+profileToggle.addEventListener('click', function () {
+	profileContent.classList.toggle('show');
+	if (profileContent.classList.contains('show')) {
+		this.firstElementChild.style.stroke = 'var(--primary-color)';
+	} else {
+		if (document.body.classList.contains('dark')) {
+			this.firstElementChild.style.stroke = 'var(--white-color)';
+		} else {
+			this.firstElementChild.style.stroke = 'var(--black-color)';
+		}
+	}
 });

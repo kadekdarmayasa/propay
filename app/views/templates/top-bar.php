55e319baaa43775a929401803334c9243b2f33f5
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-02-28 10:31:40 +0800

    Finishing styling of topbar notifcation and profile toggler

--- app/views/templates/top-bar.php
+++ app/views/templates/top-bar.php
@@ -4,4 +4,16 @@
       <span></span>
       <span></span>
     </div>
+    <div class="notification-profile">
+      <div class="notification">
+        <svg width="24" height="20" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M5.35714 14.0263L3 16.3947H10.6829L10.5303 17.0448C10.2959 18.0436 11.0538 19 12.0797 19C13.053 19 13.7983 18.1342 13.6535 17.1717L13.5366 16.3947H21L18.8571 14.0263V7.75C18.8571 4.02208 15.8351 1 12.1071 1C8.37922 1 5.35714 4.02208 5.35714 7.75V14.0263Z" stroke="#152A4A" stroke-width="2" />
+        </svg>
+        <span>12</span>
+      </div>
+      <hr>
+      <svg class="profile-toggle" width="9" height="7" viewBox="0 0 9 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1 1L4.5828 6L8 1" stroke="#152A4A" stroke-linecap="round" />
+      </svg>
+    </div>
   </div>

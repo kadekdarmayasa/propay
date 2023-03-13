94b407afe13ef0d472a36efeaafc8d910894bac4
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-03-13 10:16:58 +0800

    Add form event listener and error message display logic for username and password validation

--- public/js/staff/index.js
+++ public/js/staff/index.js
@@ -2,6 +2,7 @@ const firstForm = document.querySelector('.form.first');
 const username = document.querySelector('.username');
 const nextBtn = document.querySelector('.next-btn');
 const prevBtn = document.querySelector('.prev-btn');
+const messages = document.querySelectorAll('.message');
 
 nextBtn.addEventListener('click', () => {
 	firstForm.classList.add('hide');
@@ -11,21 +12,53 @@ prevBtn.addEventListener('click', () => {
 	firstForm.classList.remove('hide');
 });
 
-username.addEventListener('keyup', function () {
-	let isValid = validateUsername(this.value);
+firstForm.addEventListener('keyup', (e) => {
+	if (e.target.classList.contains('username')) {
+		let isValid = validateUsername(e.target.value);
 
-	if (!isValid) {
-		document.querySelector('.next-btn').disabled = true;
-		document.querySelector('.username').classList.add('error');
-		document.getElementById('message').innerText = 'Please enter a valid username';
-	} else {
-		checkAvailability(this.value);
+		if (!isValid) {
+			document.querySelector('.next-btn').disabled = true;
+			e.target.classList.add('error');
+			showErrorMessage('username-message', 'Please enter a valid username');
+		} else {
+			checkAvailability(e.target.value);
+		}
+	}
+
+	if (e.target.classList.contains('password')) {
+		let isValid = validatePassword(e.target.value);
+
+		if (!isValid) {
+			document.querySelector('.next-btn').disabled = true;
+			e.target.classList.add('error');
+			showErrorMessage('password-message', 'The password must start with alphabet and followed by (7-14) characters except space');
+		} else {
+			document.querySelector('.next-btn').disabled = false;
+			e.target.classList.remove('error');
+			showErrorMessage('password-message', '');
+		}
 	}
 });
+
+function showErrorMessage(classNm, errorMessage) {
+	messages.forEach((message) => {
+		if (message.classList.contains(classNm)) {
+			message.innerText = errorMessage;
+		}
+	});
+}
 
 function validateUsername(username) {
 	var nameRegex = /^([a-z0-9-_]+)$/i;
 	var isValid = username.match(nameRegex);
+	return isValid;
+}
+
+function validatePassword(password) {
+	// This regex will check valid password
+	// which is start from alphabet and followed by 7 to 14 characters
+	var passwordRegex = /^[A-Za-z]\w{7,14}$/;
+	var isValid = password.match(passwordRegex);
 	return isValid;
 }
 
@@ -41,11 +74,11 @@ function checkAvailability(str) {
 			if (data.status == 'error') {
 				document.querySelector('.next-btn').disabled = true;
 				document.querySelector('.username').classList.add('error');
-				document.getElementById('message').innerText = data.message;
+				showErrorMessage('username-message', data.message);
 			} else {
 				document.querySelector('.next-btn').disabled = false;
 				document.querySelector('.username').classList.remove('error');
-				document.getElementById('message').innerText = '';
+				showErrorMessage('username-message', '');
 			}
 		}
 	};

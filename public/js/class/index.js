899de9afe510752c10e6b4cf797c3682af78b0c3
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-03-16 17:05:45 +0800

    Update index.js to add import, display success message on delete, and add click event listener to page selected container

--- public/js/class/index.js
+++ public/js/class/index.js
@@ -1,16 +1,17 @@
-const pageSelectedContainer = document.querySelector('.page-selected');
-
-pageSelectedContainer.addEventListener('click', () => {
-	document.querySelector('.list-of-page').classList.toggle('show');
-});
+import '../components/overlay.js';
 
 const deleteButtons = document.querySelectorAll('.delete-btn');
-const updateButtons = document.querySelectorAll('.update-btn');
 const overlay = document.querySelector('.overlay');
 const overlayCancelBtn = document.getElementById('overlay-cancel-btn');
 const overlayDeleteBtn = document.getElementById('overlay-delete-btn');
 const overlayMetaTitle = document.querySelector('.overlay .title');
 const overlayMetaDescription = document.querySelector('.overlay .description');
+const icon = document.querySelector('.icon');
+const pageSelectedContainer = document.querySelector('.page-selected');
+
+pageSelectedContainer.addEventListener('click', () => {
+	document.querySelector('.list-of-page').classList.toggle('show');
+});
 
 overlay.addEventListener('click', function () {
 	overlay.classList.remove('show');
@@ -18,8 +19,12 @@ overlay.addEventListener('click', function () {
 
 deleteButtons.forEach((button) => {
 	button.addEventListener('click', function (e) {
-		overlayMetaTitle.textContent = 'Are you sure?';
-		overlayMetaDescription.textContent = 'This data will be permanently deleted';
+		icon.innerHTML = /* html */ `
+			<svg width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<circle cx="16" cy="23.5" r="1" fill="black" />
+				<path d="M12 13V13C12 10.7909 13.7909 9 16 9H17V9C19.4853 9 21.5 11.0147 21.5 13.5V13.5C21.5 15.9853 19.4853 18 17 18H16V21.5" stroke="black" />
+				<rect x="0.5" y="0.5" width="32" height="32" rx="16" stroke="black" />
+			</svg>`;
 
 		overlay.classList.add('show');
 
@@ -30,6 +35,36 @@ deleteButtons.forEach((button) => {
 
 		e.preventDefault();
 	});
+});
+
+overlayDeleteBtn.addEventListener('click', function (e) {
+	e.preventDefault();
+
+	const url = this.getAttribute('href');
+	fetch(url, {
+		mode: 'no-cors',
+		credentials: 'same-origin',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			icon.innerHTML = /* html */ `
+				<svg width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<rect x="0.5" y="0.5" width="32" height="32" rx="16" stroke="black"/>
+					<path d="M9.5 15L15.5 22.5L24 10.5" stroke="black" stroke-linecap="round"/>
+				</svg>
+`;
+			overlayMetaTitle.textContent = 'Congratulations';
+			overlayMetaDescription.textContent = ` Class ${data.class_name} has been successfully deleted`;
+			overlay.classList.add('show');
+			overlayDeleteBtn.style.display = 'none';
+			overlayCancelBtn.textContent = 'Close';
+			overlayCancelBtn.addEventListener('click', () => {
+				location.reload();
+			});
+		});
 });
 
 overlayCancelBtn.addEventListener('click', (e) => {

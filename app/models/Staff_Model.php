0f429eb7fd1923f7402a3153327413ca0b49608d
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-03-17 09:08:08 +0800

    Update Staff_Model to add methods for getting staff by id and updating staff data

--- app/models/Staff_Model.php
+++ app/models/Staff_Model.php
@@ -14,7 +14,18 @@ class Staff_Model
     $query = 'SELECT * FROM ' . $this->table . ' WHERE username = :username';
 
     $this->db->query($query);
-    $this->db->bind(':username', $username);
+    $this->db->bind('username', $username);
+    $this->db->execute();
+
+    return $this->db->single();
+  }
+
+  public function getStaffById($staff_id)
+  {
+    $query = 'SELECT * FROM ' . $this->table . ' WHERE staff_id = :staff_id';
+
+    $this->db->query($query);
+    $this->db->bind('staff_id', $staff_id);
     $this->db->execute();
 
     return $this->db->single();
@@ -43,13 +54,13 @@ class Staff_Model
     $query = "INSERT INTO tb_staff VALUES(null, :username, :password, :staff_level, :staff_name, :date_of_birth, :religion, :address)";
 
     $this->db->query($query);
-    $this->db->bind(':username', $username);
-    $this->db->bind(':password', $password);
-    $this->db->bind(':staff_level', $staff_level);
-    $this->db->bind(':staff_name', $staff_name);
-    $this->db->bind(':date_of_birth', $date_of_birth);
-    $this->db->bind(':religion', $religion);
-    $this->db->bind(':address', $address);
+    $this->db->bind('username', $username);
+    $this->db->bind('password', $password);
+    $this->db->bind('staff_level', $staff_level);
+    $this->db->bind('staff_name', $staff_name);
+    $this->db->bind('date_of_birth', $date_of_birth);
+    $this->db->bind('religion', $religion);
+    $this->db->bind('address', $address);
     $this->db->execute();
 
     return [
@@ -67,5 +78,40 @@ class Staff_Model
     $this->db->execute();
 
     return $this->db->rowCount();
+  }
+
+  public function updateStaff($data)
+  {
+    $staff_id = $data['staff_id'];
+    $username = htmlspecialchars($data['username']);
+    if (count(explode('\'\'', $data['staff-password'])) < 40) {
+      $password = password_hash($data['staff-password'], PASSWORD_BCRYPT);
+    } else {
+      $password = $data['staff-password'];
+    }
+    $staff_level = htmlspecialchars($data['staff-level']);
+    $staff_name = htmlspecialchars($data['staff-name']);
+    $date_of_birth = htmlspecialchars($data['date-of-birth']);
+    $religion = htmlspecialchars($data['religion']);
+    $address = htmlspecialchars($data['address']);
+
+    $query = "UPDATE " . $this->table . " SET username=:username, password=:password, staff_level=:staff_level, staff_name=:staff_name, date_of_birth=:date_of_birth, religion=:religion, address=:address WHERE
+    staff_id=:staff_id";
+
+    $this->db->query($query);
+    $this->db->bind('staff_id', $staff_id);
+    $this->db->bind('username', $username);
+    $this->db->bind('password', $password);
+    $this->db->bind('staff_level', $staff_level);
+    $this->db->bind('staff_name', $staff_name);
+    $this->db->bind('date_of_birth', $date_of_birth);
+    $this->db->bind('religion', $religion);
+    $this->db->bind('address', $address);
+    $this->db->execute();
+
+    return [
+      'row_count' => $this->db->rowCount(),
+      'last_id' => $staff_id
+    ];
   }
 }

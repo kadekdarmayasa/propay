e4b5a9829932844d1296b6191d77fdff97c95064
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-03-17 14:11:43 +0800

    Add staff detail view with authentication information

---
+++ app/views/staff/detail.php
@@ -0,0 +1,43 @@
+<div class="main-content">
+  <div class="detail-staff-container">
+    <div class="left-content">
+      <h2 class="header">General Information</h2>
+      <div class="field">
+        <small class="name">Name</small>
+        <p class="value"><?= $data['staff']['staff_name'] ?></p>
+      </div>
+      <div class="field">
+        <small class="name">Date Of Birth</small>
+        <p class="value">
+          <?= date('d F Y', strtotime($data['staff']['date_of_birth'])); ?>
+        </p>
+      </div>
+      <div class="field">
+        <small class="name">Religion</small>
+        <p class="value"><?= $data['staff']['religion']; ?></p>
+      </div>
+      <div class="field">
+        <small class="name">Address</small>
+        <p class="value"><?= $data['staff']['address']; ?></p>
+      </div>
+    </div>
+
+    <div class="right-content">
+      <h2 class="header">Authentication Information</h2>
+      <div class="field">
+        <small class="name">Username</small>
+        <p class="value"><?= $data['staff']['username']; ?></p>
+      </div>
+      <div class="field">
+        <small class="name">Staff Level</small>
+        <p class="value"><?= $data['staff']['staff_level']; ?></p>
+      </div>
+      <div class="field">
+        <small class="name">Password</small>
+        <p class="value password">
+          <?= $data['staff']['password']; ?>
+        </p>
+      </div>
+    </div>
+  </div>
+</div>

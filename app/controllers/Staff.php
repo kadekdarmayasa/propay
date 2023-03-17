b5c327fc941d0c8465ca731c76773369727727f6
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-03-17 09:07:38 +0800

    Add update and update_staff methods to Staff controller

--- app/controllers/Staff.php
+++ app/controllers/Staff.php
@@ -106,4 +106,51 @@ class Staff extends Controller
       ]));
     }
   }
+
+  public function update($staff_id)
+  {
+    if (!isset($_SESSION['user'])) {
+      header('Location: ' . BASEURL . 'auth/login');
+      exit;
+    }
+
+    if ($_SESSION['user']['staff_level'] == 'admin' || $_SESSION['user']['staff_level'] == 'staff') {
+      $staff_name = $_SESSION['user']['staff_name'];
+      $data['name'] = $staff_name;
+      $data['role'] = $_SESSION['user']['staff_level'];
+    }
+
+    $data['staff'] = $this->model('Staff_Model')->getStaffById($staff_id);
+    $data['title'] = 'Propay - Staff';
+    $data['breadcrumb'] = 'Staff/Update';
+    $data['religions'] = ['Hindu', 'Islam', 'Christian', 'Buddha', 'Kong Hu Cu'];
+    $this->view('templates/header', $data, 'staff/update');
+    $this->view('templates/sidebar', $data, 'staff/update');
+    $this->view('templates/top-bar', $data, 'staff/update');
+    $this->view('staff/update', $data, 'staff/update');
+    $this->view('templates/footer', $data, 'staff/update');
+  }
+
+  public function update_staff()
+  {
+    $json = file_get_contents('php://input');
+    $data = json_decode($json, true);
+    $result = $this->model("Staff_Model")->updateStaff($data);
+
+    if ($result['row_count'] > 0) {
+      $response = [
+        'status' => 'success',
+        'message' => 'Staff has been successfully update',
+        'id_staff' =>  $result['last_id'],
+        'url' => BASEURL . 'staff/index'
+      ];
+      file_put_contents('php://output', json_encode($response));
+    } else {
+      $response = [
+        'status' => 'error',
+        'message' => 'Failed to update staff'
+      ];
+      file_put_contents('php://output', json_encode($response));
+    }
+  }
 }

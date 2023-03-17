a7cb7227d0efb4e71f5022bc3d6f08421dc787c3
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-03-17 09:09:11 +0800

    <path d="M17.7727 7.19231L19.5909 9.51489" stroke="#989898" stroke-linecap="round" />                     <rect x="6.97767" y="13.8462" width="14.0222" height="3.15385" rx="1.07692" stroke="#989898" />                   </svg>                 </a>               </td>Update staff index page to link to update staff page

--- app/views/staff/index.php
+++ app/views/staff/index.php
@@ -71,7 +71,7 @@
             <td><?= $data['staff'][$i]['staff_name']; ?></td>
             <?php if ($_SESSION['user']['username'] != $data['staff'][$i]['username']) : ?>
               <td>
-                <a href="" class="update-btn">
+                <a href="<?= BASEURL . 'staff/update/' . $data['staff'][$i]['staff_id'] ?>" class="update-btn">
                   <svg width="21" height="19" viewBox="0 0 21 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M4.63638 12.7742L5.21649 10.4983C5.27328 10.2755 5.40513 10.0791 5.58984 9.94209L17.691 0.967977C18.415 0.431087 19.4359 0.57489 19.9834 1.29088C20.5424 2.02193 20.3949 3.06908 19.6557 3.61727L7.57334 12.5774C7.40103 12.7052 7.19219 12.7742 6.97767 12.7742H4.63638Z" stroke="#989898" stroke-linecap="round" />
                     <path d="M16.4545 2.03223L18.2727 4.35481" stroke="#989898" stroke-linecap="round" />

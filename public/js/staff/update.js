10bf22e0abf9ff2b94585efde1d19ca01ac7049b
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-03-19 12:55:35 +0800

    Remove CompletedIllustration component from public/js

--- public/js/staff/update.js
+++ public/js/staff/update.js
@@ -1,10 +1,6 @@
 import '../components/illustration.js';
 
-const firstForm = document.querySelector('.form.first');
-const nextBtn = document.querySelector('.next-btn');
-const prevBtn = document.querySelector('.prev-btn');
 const submitBtn = document.querySelector('.submit-btn');
-const messages = document.querySelectorAll('.message');
 const illustrationComponent = document.querySelector('illustration-element');
 const illustrationImage = document.querySelector('.illustration-image');
 const url = location.href.split('staff', 1).toString();
@@ -70,106 +66,6 @@ submitBtn.addEventListener('click', async (e) => {
 		}
 	}
 });
-
-nextBtn.addEventListener('click', (e) => {
-	e.preventDefault();
-
-	const firstFormInputs = document.querySelectorAll('.first.form input');
-	const emptyFields = Array.from(firstFormInputs).filter((input) => input.value == '');
-	if (emptyFields.length !== 0) {
-		nextBtn.disabled = true;
-	} else {
-		firstForm.classList.add('hide');
-	}
-});
-
-prevBtn.addEventListener('click', () => {
-	firstForm.classList.remove('hide');
-});
-
-firstForm.addEventListener('keyup', (e) => {
-	if (e.target.classList.contains('username')) {
-		let isValid = validateUsername(e.target.value);
-
-		if (!isValid) {
-			nextBtn.disabled = true;
-			e.target.classList.add('error');
-			showErrorMessage('username-message', 'Please enter a valid username');
-		} else {
-			checkAvailability(e.target.value);
-		}
-	}
-
-	if (e.target.classList.contains('password')) {
-		let result = validatePassword(e.target.value);
-
-		if (!result['isValid']) {
-			nextBtn.disabled = true;
-			e.target.classList.add('error');
-			showErrorMessage('password-message', result['errorMessage']);
-		} else {
-			nextBtn.disabled = false;
-			e.target.classList.remove('error');
-		}
-	}
-
-	if (e.target.classList.contains('confirm-password')) {
-		let password = document.querySelector('.password');
-
-		if (e.target.value !== password.value) {
-			document.querySelector('.next-btn').disabled = true;
-			e.target.classList.add('error');
-			showErrorMessage('confirm-password-message', `Password doesn't match`);
-		} else {
-			document.querySelector('.next-btn').disabled = false;
-			e.target.classList.remove('error');
-		}
-	}
-});
-
-function showErrorMessage(classNm, errorMessage) {
-	messages.forEach((message) => {
-		if (message.classList.contains(classNm)) {
-			message.innerText = errorMessage;
-		}
-	});
-}
-
-function validateUsername(username) {
-	let nameRegex = /^([a-z0-9-_]+)$/i;
-	let isValid = username.match(nameRegex);
-	return isValid;
-}
-
-function validatePassword(password) {
-	let passwordRegex = /^[A-Za-z]\w{7,20}$/;
-	let isValid = password.match(passwordRegex);
-	return { isValid, errorMessage: 'The password must start with alphabet and followed by (7-20) characters' };
-}
-
-function checkAvailability(str) {
-	const data = { username: str };
-
-	const xHttp = new XMLHttpRequest();
-	xHttp.open('POST', 'http://localhost/propay-payment-system/staff/check_staff', true);
-
-	xHttp.onreadystatechange = function () {
-		if (this.readyState == 4 && this.status == 200) {
-			const data = JSON.parse(this.responseText);
-			if (data.status == 'error') {
-				document.querySelector('.next-btn').disabled = true;
-				document.querySelector('.username').classList.add('error');
-				showErrorMessage('username-message', data.message);
-			} else {
-				document.querySelector('.next-btn').disabled = false;
-				document.querySelector('.username').classList.remove('error');
-			}
-		}
-	};
-
-	xHttp.setRequestHeader('Content-type', 'application/json');
-	xHttp.send(JSON.stringify(data));
-}
 
 async function updateStaff(staff_data) {
 	const data = staff_data;

0665ea88776966194b8adf24e13e2daeec79ae44
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-03-19 12:08:46 +0800

    Update view to use illustration-element instead of completed-illustration

--- app/views/class/add.php
+++ app/views/class/add.php
@@ -40,6 +40,6 @@
       </form>
     </div>
 
-    <completed-illustration src="<?= BASEURL . 'public/images/completed.svg' ?>"></completed-illustration>
+    <illustration-element></illustration-element>
   </div>
 </div>

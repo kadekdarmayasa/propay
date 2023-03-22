a96ef6d54f57bb4c45da6da2394c04c42b818c93
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-03-22 16:46:42 +0800

    Add method page on staff controllers for pagination

--- app/controllers/Staff.php
+++ app/controllers/Staff.php
@@ -5,23 +5,11 @@ class Staff extends Controller
   {
     if (!isset($_SESSION['user'])) {
       header('Location: ' . BASEURL . 'auth/login');
-      exit;
+    } else {
+      header('Location: ' . BASEURL . 'staff/page/1');
     }
 
-    if ($_SESSION['user']['staff_level'] == 'admin' || $_SESSION['user']['staff_level'] == 'staff') {
-      $staff_name = $_SESSION['user']['staff_name'];
-      $data['name'] = $staff_name;
-      $data['role'] = $_SESSION['user']['staff_level'];
-    }
-
-    $data['title'] = 'Propay - Staff';
-    $data['breadcrumb'] = 'Staff';
-    $data['staff'] = $this->model('Staff_Model')->getAllStaff();
-    $this->view('templates/header', $data, 'staff/index');
-    $this->view('templates/sidebar', $data, 'staff/index');
-    $this->view('templates/top-bar', $data, 'staff/index');
-    $this->view('staff/index', $data, 'staff/index');
-    $this->view('templates/footer', $data, 'staff/index');
+    exit;
   }
 
   public function add()
@@ -37,6 +25,7 @@ class Staff extends Controller
       $data['role'] = $_SESSION['user']['staff_level'];
     }
 
+    $data['page'] = $_SESSION['active_page'];
     $data['title'] = 'Propay - Staff';
     $data['breadcrumb'] = 'Staff/Add';
     $this->view('templates/header', $data, 'staff/add');
@@ -121,6 +110,7 @@ class Staff extends Controller
     }
 
     $data['staff'] = $this->model('Staff_Model')->getStaffById($staff_id);
+    $data['page'] = $_SESSION['active_page'];
     $data['title'] = 'Propay - Staff';
     $data['breadcrumb'] = 'Staff/Update';
     $data['religions'] = ['Hindu', 'Islam', 'Christian', 'Buddha', 'Kong Hu Cu'];
@@ -176,6 +166,7 @@ class Staff extends Controller
       $data['role'] = $_SESSION['user']['staff_level'];
     }
 
+    $data['page'] = $_SESSION['active_page'];
     $data['title'] = 'Propay - Staff';
     $data['breadcrumb'] = 'Staff/Detail';
     $data['staff'] =  $this->model("Staff_Model")->getStaffById($staff_id);
@@ -184,5 +175,111 @@ class Staff extends Controller
     $this->view('templates/top-bar', $data, 'staff/detail');
     $this->view('staff/detail', $data, 'staff/detail');
     $this->view('templates/footer', $data, 'staff/detail');
+  }
+
+  public function page($page)
+  {
+    if ($page < 1) {
+      header('Location: ' . BASEURL . 'staff/index');
+      exit;
+    }
+
+    if ($_SESSION['user']['staff_level'] == 'admin' || $_SESSION['user']['staff_level'] == 'staff') {
+      $staff_name = $_SESSION['user']['staff_name'];
+      $data['name'] = $staff_name;
+      $data['role'] = $_SESSION['user']['staff_level'];
+    }
+
+    // Pagination
+    if (isset($_SESSION['search_keyword']) && $_SESSION['search_keyword'] != '') {
+      $total_data = count($this->model('Staff_Model')->getStaffByAny($_SESSION['search_keyword']));
+      $data['staff_amount'] = $total_data;
+    } else {
+      $total_data = count($this->model('Staff_Model')->getAllStaff());
+      $data['staff_amount'] = $total_data;
+    }
+
+
+    if (isset($_POST['search-staff'])) {
+      $staff = $this->model('Staff_Model')->getStaffByAny($_POST['staff-field']);
+      $total_data = count($staff);
+      $data['staff_amount'] = $total_data;
+      $_SESSION['search_keyword'] = $_POST['staff-field'];
+
+      if ($total_data < 6) {
+        header('location: ' . BASEURL . 'staff/index');
+        exit;
+      }
+    }
+
+    if (isset($_POST['row_per_page'])) {
+      $total_data_per_page = $_POST['row_per_page'];
+      $_SESSION['row_per_page'] = $_POST['row_per_page'];
+    } else {
+      $total_data_per_page = isset($_SESSION['row_per_page']) ? $_SESSION['row_per_page'] : 5;
+    }
+
+    $total_page = ceil($total_data / $total_data_per_page);
+
+    if ($total_page < 2 && $page != 1) {
+      header('Location: ' . BASEURL . 'staff/index');
+    }
+
+    if ($page > $total_page) {
+      header('Location: ' .  BASEURL . 'staff/page/' . $total_page);
+      exit;
+    }
+
+    $current_page = $page;
+    $start_data = ($total_data_per_page * $current_page) - $total_data_per_page;
+    $end_data = $start_data + $total_data_per_page;
+    $total_link = 2;
+
+    if ($current_page > $total_link) {
+      $start_number = $current_page - $total_link;
+    } else {
+      $start_number = 1;
+    }
+
+    if ($current_page < ($total_page - $total_link)) {
+      $end_number = $current_page + $total_link;
+    } else {
+      $end_number = $total_page;
+    }
+
+    if ($end_number != $total_page) {
+      $start_number = $current_page - $total_link + 1;
+      if ($start_number < 1) {
+        $start_number = 1;
+      }
+    }
+
+    if (isset($_SESSION['search_keyword'])) {
+      $data['staff'] = $this->model('Staff_Model')->getStaffWithLimit($start_data, $total_data_per_page, $_SESSION['search_keyword']);
+      $data['staff_amount'] = count($data['staff']);
+    } else {
+      $data['staff'] = $this->model('Staff_Model')->getStaffWithLimit($start_data, $total_data_per_page);
+      $data['staff_amount'] = count($data['staff']);
+    }
+
+    $data['pagination'] = [
+      'total_page' => $total_page,
+      'current_page' => $current_page,
+      'start_number' => $start_number,
+      'end_number' => $end_number,
+      'total_link' => $total_link,
+      'start_data' => $start_data,
+      'end_data' => $end_data,
+    ];
+
+    $_SESSION['active_page'] = $data['pagination']['current_page'];
+    $data['title'] = 'Propay - Staff';
+    $data['breadcrumb'] = 'Staff';
+    $data['keyword'] = $_SESSION['search_keyword'] ?? '';
+    $this->view('templates/header', $data, 'staff/index');
+    $this->view('templates/sidebar', $data, 'staff/index');
+    $this->view('templates/top-bar', $data, 'staff' . $page . '/index');
+    $this->view('staff/index', $data, 'staff/index');
+    $this->view('templates/footer', $data, 'staff/index');
   }
 }

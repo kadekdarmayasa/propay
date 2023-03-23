b3d4fb202d727669f9535662414353d9852a2a61
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-03-23 11:12:37 +0800

    Update checkAvailability() to trim user input and add classNames object for dynamic selectors

--- public/js/helpers/check_availability.js
+++ public/js/helpers/check_availability.js
@@ -1,25 +1,30 @@
 import { errorMessage } from './error_message.js';
 
 function checkAvailability(name, value, url) {
-	const data = { [name]: value };
+	const data = { [name]: value.trim() };
 
 	const xHttp = new XMLHttpRequest();
-	xHttp.open('POST', url, true);
 
-	xHttp.onreadystatechange = function () {
+	xHttp.onreadystatechange = async function () {
 		if (this.readyState == 4 && this.status == 200) {
 			const data = JSON.parse(this.responseText);
+			const classNames = {
+				username: '.next-btn',
+				'class-name': '.submit-btn',
+			};
+
 			if (data.status == 'error') {
-				document.querySelector('.next-btn').disabled = true;
+				document.querySelector(`${classNames[name]}`).disabled = true;
 				document.querySelector(`.${name}`).classList.add('error');
 				errorMessage(`${name}-message`, data.message);
 			} else {
-				document.querySelector('.next-btn').disabled = false;
+				document.querySelector(`${classNames[name]}`).disabled = false;
 				document.querySelector(`.${name}`).classList.remove('error');
 			}
 		}
 	};
 
+	xHttp.open('POST', url, true);
 	xHttp.setRequestHeader('Content-type', 'application/json');
 	xHttp.send(JSON.stringify(data));
 }

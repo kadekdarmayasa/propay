2ce22982ec4b762b26d3c8380ffd7f2959c65679
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-03-25 10:49:13 +0800

    Create Payment_Model class for adding payment information

---
+++ app/models/Payment_Model.php
@@ -0,0 +1,26 @@
+<?php
+class Payment_Model
+{
+  private $table = "payment";
+  private $db;
+
+  public function __construct()
+  {
+    $this->db = new Database();
+  }
+
+  public function addPayment($edc_id, $sin, $year, $month, $due_date)
+  {
+    $query = "INSERT INTO " . $this->table . " (month, year, due_date, sin, edc_id) VALUES (:month, :year, :due_date, :sin, :edc_id)";
+
+    $this->db->query($query);
+    $this->db->bind(':month', $month);
+    $this->db->bind(':year', $year);
+    $this->db->bind(':due_date', $due_date);
+    $this->db->bind(':sin', $sin);
+    $this->db->bind(':edc_id', $edc_id);
+    $this->db->execute();
+
+    return $this->db->rowCount();
+  }
+}

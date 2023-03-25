af39206f3e8264a13b1ac40f4b06795a84404319
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-03-25 16:35:17 +0800

    Remove unused CSS and JS helpers for Student Add page

--- public/js/helpers/password_validation.js
+++
@@ -1,7 +0,0 @@
-function validatePassword(password) {
-	let passwordRegex = /^[A-Za-z]\w{7,20}$/;
-	let isValid = password.match(passwordRegex);
-	return { isValid, errorMessage: 'The password must start with alphabet and followed by (7-20) characters' };
-}
-
-export { validatePassword };
--- public/js/helpers/username_validation.js
+++
@@ -1,7 +0,0 @@
-function validateUsername(username) {
-	let nameRegex = /^([a-z0-9-_]+)$/i;
-	let isValid = username.match(nameRegex);
-	return isValid;
-}
-
-export { validateUsername };

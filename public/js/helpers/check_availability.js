c9af96e73d88e4020802a0b83b927997f18240af
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-03-25 10:41:40 +0800

    Remove classNames object to simplify checkAvailability function

--- public/js/helpers/check_availability.js
+++ public/js/helpers/check_availability.js
@@ -8,17 +8,11 @@ function checkAvailability(name, value, url) {
 	xHttp.onreadystatechange = async function () {
 		if (this.readyState == 4 && this.status == 200) {
 			const data = JSON.parse(this.responseText);
-			const classNames = {
-				username: '.next-btn',
-				'class-name': '.submit-btn',
-			};
 
 			if (data.status == 'error') {
-				document.querySelector(`${classNames[name]}`).disabled = true;
 				document.querySelector(`.${name}`).classList.add('error');
 				errorMessage(`${name}-message`, data.message);
 			} else {
-				document.querySelector(`${classNames[name]}`).disabled = false;
 				document.querySelector(`.${name}`).classList.remove('error');
 			}
 		}

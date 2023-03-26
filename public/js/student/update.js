98de0ba0e59c4533fb2e4c07eca10826f46612f9
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-03-26 11:46:04 +0800

    Update validator and illustration components for student update page

--- public/js/student/update.js
+++ public/js/student/update.js
@@ -1,28 +1,25 @@
 import '../components/illustration.js';
+import validateInputs from '../helpers/validator/index.js';
+import { showIllustrationComp, prepIllustrationComp } from '../helpers/illustration.js';
 
 const form = document.querySelector('.form');
 const submitBtn = document.querySelector('.submit-btn');
-const illustrationComponent = document.querySelector('illustration-element');
 const url = location.href.split('student', 1).toString();
 
-form.addEventListener('keyup', function () {
-	validateAllInputs(this);
+form.addEventListener('keyup', function (e) {
+	const isContainError = validateInputs(e.target, 'update-student', this);
+	submitBtn.style.visibility = isContainError ? 'hidden' : 'visible';
 });
 
-form.addEventListener('change', function () {
-	validateAllInputs(this);
+form.addEventListener('input', function (e) {
+	const isContainError = validateInputs(e.target, 'update-student', this);
+	submitBtn.style.visibility = isContainError ? 'hidden' : 'visible';
 });
 
-function validateAllInputs(element) {
-	const inputs = Array.from(element.querySelectorAll('.input'));
-	const isContainError = inputs.some((input) => input.value == '');
-
-	if (!isContainError) {
-		submitBtn.style.visibility = 'visible';
-	} else {
-		submitBtn.style.visibility = 'hidden';
-	}
-}
+form.addEventListener('change', function (e) {
+	const isContainError = validateInputs(e.target, 'update-student', this);
+	submitBtn.style.visibility = isContainError ? 'hidden' : 'visible';
+});
 
 submitBtn.addEventListener('click', async (e) => {
 	e.preventDefault();
@@ -39,46 +36,34 @@ submitBtn.addEventListener('click', async (e) => {
 		document.querySelector('form').reset();
 		document.querySelector('.first-content').style.display = 'none';
 
-		const title = 'Congratulations';
-		const message = response.message;
-		const description = `
-      You just updated student with sin ${response.sin},<br>lets see the list of student by clicking the button below
-    `;
-		const view = 'student';
-		const lisOfStudentUrl = response.url;
-		const imgSource = `${url}public/images/completed.svg`;
+		const illustrationProps = {
+			title: 'Congratulations',
+			message: response.message,
+			description: `
+				You just updated student with sin ${response.sin},<br>lets see the list of student by clicking the button below
+			`,
+			view: 'student',
+			redirectUrl: response.url,
+			illustrationImage: `${url}public/images/completed.svg`,
+		};
 
-		illustrationComponent.setAttribute('title', title);
-		illustrationComponent.setAttribute('message', message);
-		illustrationComponent.setAttribute('src', imgSource);
-		illustrationComponent.setAttribute('description', description);
-		illustrationComponent.setAttribute('view', view);
-		illustrationComponent.setAttribute('url', lisOfStudentUrl);
-		illustrationComponent.firstElementChild.style.opacity = '1';
-		illustrationComponent.firstElementChild.style.display = 'flex';
+		showIllustrationComp(prepIllustrationComp(illustrationProps), 'success');
 	} else if (response.status == 'nothing-update') {
 		document.querySelector('form').reset();
 		document.querySelector('.first-content').style.display = 'none';
 
-		const title = 'No Data Change';
-		const message = response.message;
-		const description = `
-			There is no student data change with sin ${response.sin},<br>lets see the list of student by clicking the button below
-		`;
-		const view = 'student';
-		const listOfStudentUrl = response.url;
-		const src = `${url}public/images/no-data-update-illustration.svg`;
+		const illustrationProps = {
+			title: 'No Data Change',
+			message: response.message,
+			description: `
+				There is no student data change with sin ${response.sin},<br>lets see the list of student by clicking the button below
+			`,
+			view: 'student',
+			redirectUrl: response.url,
+			illustrationImage: `${url}public/images/no-data-update-illustration.svg`,
+		};
 
-		illustrationComponent.setAttribute('title', title);
-		illustrationComponent.setAttribute('message', message);
-		illustrationComponent.setAttribute('description', description);
-		illustrationComponent.setAttribute('view', view);
-		illustrationComponent.setAttribute('url', listOfStudentUrl);
-		illustrationComponent.setAttribute('src', src);
-
-		illustrationComponent.firstElementChild.style.opacity = '1';
-		illustrationComponent.firstElementChild.style.display = 'flex';
-		illustrationImage.style.width = '40%';
+		showIllustrationComp(prepIllustrationComp(illustrationProps), 'nothing-update');
 	}
 });
 

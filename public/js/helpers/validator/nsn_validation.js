84a85e0028edf2c749748f7031a2718678bad013
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-03-26 13:52:04 +0800

    Update NSN validation to allow 10 or 11 digits and reject all zeros

--- public/js/helpers/validator/nsn_validation.js
+++ public/js/helpers/validator/nsn_validation.js
@@ -1,7 +1,13 @@
 function validateNsn(nsn) {
-	const nsnRegex = /^[0-9]{10}$/;
+	const nsnRegex = /^[0-9]{10,11}$/i;
 	let isValid = nsnRegex.test(nsn);
-	return { isValid, errorMessage: 'Please enter a valid NSN' };
+	if (isValid == false) {
+		return { isValid, errorMessage: 'Please enter a valid NSN' };
+	}
+	if (nsn == '0000000000' || nsn == '00000000000') {
+		return { isValid: false, errorMessage: 'NSN cannot be all zeros' };
+	}
+	return { isValid: true, errorMessage: '' };
 }
 
 export { validateNsn };

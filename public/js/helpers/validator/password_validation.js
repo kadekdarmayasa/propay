29db411e3ea81eb7f212cea20c9167b8a06191f6
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-03-26 13:43:20 +0800

    Update password validation regex and error message

--- public/js/helpers/validator/password_validation.js
+++ public/js/helpers/validator/password_validation.js
@@ -1,7 +1,7 @@
 function validatePassword(password) {
-	let passwordRegex = /^[A-Za-z]\w{7,20}$/;
+	let passwordRegex = /^[1-9A-Za-z]\w{7,20}$/;
 	let isValid = password.match(passwordRegex);
-	return { isValid, errorMessage: 'The password must start with alphabet and followed by (7-20) characters' };
+	return { isValid, errorMessage: 'Password must contains at least 8 characters exclude space' };
 }
 
 export { validatePassword };

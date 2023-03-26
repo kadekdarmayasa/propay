3c5c169657d1e825782ec5603509a6ab82301e82
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-03-26 10:26:13 +0800

    Add logic to validate form inputs and display illustration component on successful submission

--- public/js/class/add.js
+++ public/js/class/add.js
@@ -1,53 +1,66 @@
 import '../components/illustration.js';
 import { checkAvailability } from '../helpers/check_availability.js';
+import { prepIllustrationComp, showIllustrationComp } from '../helpers/illustration.js';
 
 const submitBtn = document.getElementById('submit-btn');
-const illustrationComponent = document.querySelector('illustration-element');
 const url = location.href.split('classes', 1).toString();
 const form = document.querySelector('form');
+submitBtn.style.visibility = 'hidden';
 
 form.addEventListener('keyup', function (e) {
+	let isContainError = validateInputs(e);
+	submitBtn.style.visibility = isContainError ? 'hidden' : 'visible';
+});
+
+form.addEventListener('input', function (e) {
+	let isContainError = validateInputs(e);
+	submitBtn.style.visibility = isContainError ? 'hidden' : 'visible';
+});
+
+form.addEventListener('change', function (e) {
+	let isContainError = validateInputs(e);
+	submitBtn.style.visibility = isContainError ? 'hidden' : 'visible';
+});
+
+function validateInputs(e) {
+	const allInputs = Array.from(document.querySelectorAll('.input'));
+
 	if (e.target.id == 'class_name') {
 		checkAvailability('class-name', e.target.value, 'http://localhost/propay/classes/check_action');
 	}
-});
+
+	const isContainError = allInputs.some((input) => input.classList.contains('error') || input.value == '');
+	return isContainError;
+}
 
 submitBtn.addEventListener('click', async (e) => {
-	const inputs = [...document.querySelectorAll('input'), ...document.querySelectorAll('select')];
-	const isEmpty = inputs.filter((input) => input.value === '');
+	e.preventDefault();
 
-	if (!isEmpty.length) {
-		e.preventDefault();
+	const inputs = Array.from(document.querySelectorAll('.input'));
 
-		const data = {};
-		inputs.forEach((input) => {
-			data[input.id] = input.value;
-		});
+	const data = {};
+	inputs.forEach((input) => {
+		data[input.id] = input.value;
+	});
 
-		const response = await insertClassToDatabase(data);
-		if (response.status == 'success') {
-			form.reset();
-			document.querySelector('.first-content').style.display = 'none';
+	const response = await insertClassToDatabase(data);
 
-			const title = 'Congratulations';
-			const message = response.message;
-			const description = `
+	if (response.status == 'success') {
+		form.reset();
+		document.querySelector('.first-content').style.display = 'none';
+
+		const illustrationProps = {
+			title: 'Congratulations',
+			message: response.message,
+			description: `
 			Class ${response.class_name} has been successfully added,<br>lets see the list of class by clicking the button below
-		`;
-			const view = 'class';
-			const listOfStaffUrl = response.url;
-			const src = `${url}public/images/completed.svg`;
+		`,
+			view: 'class',
+			redirectUrl: response.url,
+			illustrationImage: `${url}public/images/completed.svg`,
+		};
 
-			illustrationComponent.setAttribute('title', title);
-			illustrationComponent.setAttribute('message', message);
-			illustrationComponent.setAttribute('description', description);
-			illustrationComponent.setAttribute('view', view);
-			illustrationComponent.setAttribute('url', listOfStaffUrl);
-			illustrationComponent.setAttribute('src', src);
-
-			illustrationComponent.firstElementChild.style.opacity = '1';
-			illustrationComponent.firstElementChild.style.display = 'flex';
-		}
+		showIllustrationComp(prepIllustrationComp(illustrationProps));
 	}
 });
 

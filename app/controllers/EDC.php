0e8c9d990a4e7735bae9d73aa58a8e9a073aaf1a
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-03-26 16:18:18 +0800

    Update EDC routes to point to the correct controller methods

--- app/controllers/EDC.php
+++ app/controllers/EDC.php
@@ -4,47 +4,27 @@ class EDC extends Controller
 {
   public function index()
   {
-    if (!isset($_SESSION['user'])) {
-      header('Location: ' . BASEURL . 'auth/login');
-      exit;
-    }
-
-    $data['title'] = 'Propay - EDC';
-    $data['breadcrumb'] = 'EDC/List';
-    $this->view('templates/header', $data, 'edc');
-    $this->view('templates/sidebar', $data, 'edc/list');
-    $this->view('templates/top-bar', $data, 'edc');
-    $this->view('edc/index', $data, 'edc');
-    $this->view('templates/footer', $data, 'edc');
+    header('Location: ' . BASEURL . 'edc_List/index');
+    exit;
   }
+
 
   public function payment()
   {
-    if (!isset($_SESSION['user'])) {
-      header('Location: ' . BASEURL . 'auth/login');
-      exit;
-    }
-
-    $data['title'] = 'Propay - EDC Payment';
-    $this->view('templates/header', $data, 'edc');
-    $this->view('templates/sidebar', $data, 'edc/payment');
-    $this->view('templates/top-bar', $data, 'edc');
-    $this->view('edc/index', $data, 'edc');
-    $this->view('templates/footer', $data, 'edc');
+    header('Location: ' . BASEURL . 'edc_payment/index');
+    exit;
   }
+
 
   public function payment_history()
   {
-    if (!isset($_SESSION['user'])) {
-      header('Location: ' . BASEURL . 'auth/login');
-      exit;
-    }
+    header('Location: ' . BASEURL . 'edc_payment_history/index');
+    exit;
+  }
 
-    $data['title'] = 'Propay - EDC Payment History';
-    $this->view('templates/header', $data, 'edc');
-    $this->view('templates/sidebar', $data, 'edc/payment_history');
-    $this->view('templates/top-bar', $data, 'edc');
-    $this->view('edc/index', $data, 'edc');
-    $this->view('templates/footer', $data, 'edc');
+  public function report()
+  {
+    header('Location: ' . BASEURL . 'edc_report/index');
+    exit;
   }
 }

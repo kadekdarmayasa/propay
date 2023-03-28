ceb5687beafab2ebd628b845c5df9bbc567ebde7
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-03-28 15:39:16 +0800

    Add payment/index.js to render waiting for search and not found illustrations

---
+++ public/js/payment/index.js
@@ -0,0 +1,33 @@
+import { showIllustrationComp, prepIllustrationComp } from '../helpers/illustration.js';
+const baseUrl = window.location.href.split('edc_payment')[0];
+
+const element = document.querySelector('.student-bills');
+if (element) {
+	element.scrollIntoView({ behavior: 'smooth' });
+}
+
+const illustration = {
+	waitingForSearch: document.getElementById('waiting-for-search-illustration'),
+	notFound: document.getElementById('not-found-illustration'),
+};
+
+const illustrationProps = {
+	waitingForSearch: {
+		view: 'waiting-for-search-payment',
+		description: 'Waiting for Search',
+		illustrationImage: `${baseUrl}public/images/search-illustration.svg`,
+	},
+	notFound: {
+		view: 'not-found-payment',
+		description: 'Not Data Student Found',
+		illustrationImage: `${baseUrl}public/images/not-found-illustration.svg`,
+	},
+};
+
+if (illustration.waitingForSearch) {
+	showIllustrationComp(prepIllustrationComp(illustrationProps.waitingForSearch));
+}
+
+if (illustration.notFound) {
+	showIllustrationComp(prepIllustrationComp(illustrationProps.notFound));
+}

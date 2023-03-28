ac2bf0f20441747c15f53cfa1320b4638dbf1538
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-03-28 21:48:23 +0800

    Update Payment_Model to add default payment status, bind parameters, and add search by keyword function

--- app/models/Payment_Model.php
+++ app/models/Payment_Model.php
@@ -11,27 +11,38 @@ class Payment_Model
 
   public function addPayment($edc_id, $sin, $year, $month, $due_date, $payment_id)
   {
-    $query = "INSERT INTO " . $this->table . " (payment_id, month, year, due_date, sin, edc_id) VALUES (:payment_id, :month, :year, :due_date, :sin, :edc_id)";
+    $query = "INSERT INTO " . $this->table . " VALUES (:payment_id, :month, :year, :due_date, 'Unpaid', :sin, :edc_id)";
 
     $this->db->query($query);
-    $this->db->bind(':payment_id', $payment_id);
-    $this->db->bind(':month', $month);
-    $this->db->bind(':year', $year);
-    $this->db->bind(':due_date', $due_date);
-    $this->db->bind(':sin', $sin);
-    $this->db->bind(':edc_id', $edc_id);
+    $this->db->bind(':payment_id', $payment_id, PDO::PARAM_INT);
+    $this->db->bind(':month', $month, PDO::PARAM_STR);
+    $this->db->bind(':year', $year, PDO::PARAM_STR);
+    $this->db->bind(':due_date', $due_date, PDO::PARAM_STR);
+    $this->db->bind(':sin', $sin, PDO::PARAM_INT);
+    $this->db->bind(':edc_id', $edc_id, PDO::PARAM_INT);
     $this->db->execute();
 
     return $this->db->rowCount();
   }
 
-  public function getPaymentWithLimit($start_data, $total_data_per_page)
+  public function getPaymentWithLimit($start_data, $total_data_per_page, $keyword = null, $sin = 0)
   {
-    $query = "SELECT * FROM " . $this->table . " LIMIT :start_data, :total_data_per_page";
+    if ($keyword != null) {
+      $query = "SELECT * FROM " . $this->table . " WHERE year LIKE :keyword OR month LIKE :keyword OR payment_status LIKE :keyword AND sin = :sin LIMIT :start_data, :total_data_per_page";
 
-    $this->db->query($query);
-    $this->db->bind(':start_data', $start_data, PDO::PARAM_INT);
-    $this->db->bind(':total_data_per_page', $total_data_per_page, PDO::PARAM_INT);
+      $this->db->query($query);
+      $this->db->bind(':keyword', '%' . $keyword . '%');
+      $this->db->bind(':sin', $sin, PDO::PARAM_INT);
+      $this->db->bind(':start_data', $start_data, PDO::PARAM_INT);
+      $this->db->bind(':total_data_per_page', $total_data_per_page, PDO::PARAM_INT);
+    } else {
+      $query = "SELECT * FROM " . $this->table . " WHERE sin = :sin LIMIT :start_data, :total_data_per_page";
+
+      $this->db->query($query);
+      $this->db->bind(':sin', $sin, PDO::PARAM_INT);
+      $this->db->bind(':start_data', $start_data, PDO::PARAM_INT);
+      $this->db->bind(':total_data_per_page', $total_data_per_page, PDO::PARAM_INT);
+    }
 
     $this->db->execute();
     return $this->db->resultSet();
@@ -42,7 +53,7 @@ class Payment_Model
     $query = "DELETE FROM " . $this->table . " WHERE sin = :sin";
 
     $this->db->query($query);
-    $this->db->bind(':sin', $sin);
+    $this->db->bind(':sin', $sin, PDO::PARAM_INT);
     $this->db->execute();
 
     return $this->db->rowCount();
@@ -53,7 +64,7 @@ class Payment_Model
     $query = "SELECT * FROM " . $this->table  .  " WHERE sin = :sin";
 
     $this->db->query($query);
-    $this->db->bind(':sin', $sin);
+    $this->db->bind(':sin', $sin, PDO::PARAM_INT);
     $this->db->execute();
 
     return $this->db->resultSet();
@@ -66,6 +77,18 @@ class Payment_Model
     $this->db->query($query);
     $this->db->execute();
 
+    return $this->db->resultSet();
+  }
+
+  public function getPaymentByAny($keyword, $sin = 0)
+  {
+    $query = "SELECT * FROM " . $this->table . " WHERE year LIKE :keyword OR month LIKE :keyword OR payment_status LIKE :keyword AND sin = :sin";
+
+    $this->db->query($query);
+    $this->db->bind(':keyword', '%' . $keyword . '%');
+    $this->db->bind(':sin', $sin, PDO::PARAM_INT);
+
+    $this->db->execute();
     return $this->db->resultSet();
   }
 }

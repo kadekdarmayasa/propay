38b6e5e8e6a24e9e88536b4df018f1558e8e9fb9
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-03-30 21:06:17 +0800

    Update rowSelectedContainer logic to accommodate EDC payment page

--- public/js/utils/row-selected.js
+++ public/js/utils/row-selected.js
@@ -2,31 +2,37 @@ const rowSelectedContainer = document.querySelector('.row-selected');
 const listOfRows = document.querySelectorAll('.list-of-row li');
 const select = document.getElementById('select');
 
-rowSelectedContainer.tabIndex = 0;
-rowSelectedContainer.firstElementChild.textContent = 5;
+if (rowSelectedContainer) {
+	rowSelectedContainer.tabIndex = 0;
+	if (location.href.split('/').includes('edc_payment')) {
+		rowSelectedContainer.firstElementChild.textContent = 6;
+	} else {
+		rowSelectedContainer.firstElementChild.textContent = 5;
+	}
 
-if (localStorage.getItem('selected')) {
-	rowSelectedContainer.firstElementChild.textContent = localStorage.getItem('selected');
-}
+	if (localStorage.getItem('selected')) {
+		rowSelectedContainer.firstElementChild.textContent = localStorage.getItem('selected');
+	}
 
-rowSelectedContainer.addEventListener('click', (e) => {
-	e.preventDefault();
-	document.querySelector('.list-of-row').classList.toggle('show');
+	rowSelectedContainer.addEventListener('click', (e) => {
+		e.preventDefault();
+		document.querySelector('.list-of-row').classList.toggle('show');
 
-	listOfRows.forEach((row) => {
-		if (row.textContent == rowSelectedContainer.firstElementChild.textContent) {
-			row.classList.add('selected');
-		} else {
-			row.classList.remove('selected');
-		}
-
-		row.addEventListener('click', (e) => {
-			select.firstElementChild.value = e.target.textContent;
-			localStorage.setItem('selected', e.target.textContent);
-
-			if (select.firstElementChild.value !== '') {
-				select.parentElement.submit();
+		listOfRows.forEach((row) => {
+			if (row.textContent == rowSelectedContainer.firstElementChild.textContent) {
+				row.classList.add('selected');
+			} else {
+				row.classList.remove('selected');
 			}
+
+			row.addEventListener('click', (e) => {
+				select.firstElementChild.value = e.target.textContent;
+				localStorage.setItem('selected', e.target.textContent);
+
+				if (select.firstElementChild.value !== '') {
+					select.parentElement.submit();
+				}
+			});
 		});
 	});
-});
+}

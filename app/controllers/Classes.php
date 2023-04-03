aa6b3cfa9a6298d4b41ed8906acf8fff86c4dd94
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-04-03 11:55:08 +0800

    Refactor code of class controllers

--- app/controllers/Classes.php
+++ app/controllers/Classes.php
@@ -13,6 +13,7 @@ class Classes extends Controller implements Actions
       unset($_SESSION['search_staff_keyword']);
       unset($_SESSION['search_student_keyword']);
       unset($_SESSION['row_per_page']);
+
       if ($_SESSION['user']['role'] == 'student' || $_SESSION['user']['role'] == 'staff') {
         header('Location: ' . BASEURL);
       }
@@ -43,6 +44,7 @@ class Classes extends Controller implements Actions
     if ($_SESSION['user']['role'] == 'admin' || $_SESSION['user']['role'] == 'staff') {
       if (isset($_SESSION['profile_change'])) {
         $staff = $this->model('Staff_Model')->getStaffById($_SESSION['user']['staff_id']);
+
         $staff_name = $staff['staff_name'];
       } else {
         $staff_name = $_SESSION['user']['staff_name'];
@@ -55,6 +57,7 @@ class Classes extends Controller implements Actions
     if ($_SESSION['user']['role'] == 'student') {
       if (isset($_SESSION['profile_change'])) {
         $student = $this->model('Student_Model')->getStudentBySIN($_SESSION['user']['sin']);
+
         $student_name = $student['student_name'];
       } else {
         $student_name = $_SESSION['user']['student_name'];
@@ -68,6 +71,7 @@ class Classes extends Controller implements Actions
     $data['breadcrumb'] = 'Classes/Add';
     $data['page'] = 1;
     $data['majors'] = ["Software Engineering", "Computer Network Engineering", "Animation", "Visual Communication Design", "Multimedia"];
+
     $this->view('templates/header', $data, 'class/add');
     $this->view('templates/sidebar', $data, 'class/add');
     $this->view('templates/top-bar', $data, 'class/add');
@@ -97,6 +101,7 @@ class Classes extends Controller implements Actions
     if ($_SESSION['user']['role'] == 'admin' || $_SESSION['user']['role'] == 'staff') {
       if (isset($_SESSION['profile_change'])) {
         $staff = $this->model('Staff_Model')->getStaffById($_SESSION['user']['staff_id']);
+
         $staff_name = $staff['staff_name'];
       } else {
         $staff_name = $_SESSION['user']['staff_name'];
@@ -109,6 +114,7 @@ class Classes extends Controller implements Actions
     if ($_SESSION['user']['role'] == 'student') {
       if (isset($_SESSION['profile_change'])) {
         $student = $this->model('Student_Model')->getStudentBySIN($_SESSION['user']['sin']);
+
         $student_name = $student['student_name'];
       } else {
         $student_name = $_SESSION['user']['student_name'];
@@ -118,11 +124,12 @@ class Classes extends Controller implements Actions
       $data['role'] = 'student';
     }
 
-    $data['class'] = $this->model("Class_Model")->getClassById($class_id);
-    $data['majors'] = ["Software Engineering", "Computer Network Engineering", "Animation", "Visual Communication Design", "Multimedia"];
     $data['title'] = 'Propay - Class';
     $data['breadcrumb'] = 'Classes/Update';
     $data['page'] = 1;
+    $data['majors'] = ["Software Engineering", "Computer Network Engineering", "Animation", "Visual Communication Design", "Multimedia"];
+    $data['class'] = $this->model("Class_Model")->getClassById($class_id);
+
     $this->view('templates/header', $data, 'class/update');
     $this->view('templates/sidebar', $data, 'class/update');
     $this->view('templates/top-bar', $data, 'class/update');
@@ -150,6 +157,7 @@ class Classes extends Controller implements Actions
     if ($_SESSION['user']['role'] == 'admin' || $_SESSION['user']['role'] == 'staff') {
       if (isset($_SESSION['profile_change'])) {
         $staff = $this->model('Staff_Model')->getStaffById($_SESSION['user']['staff_id']);
+
         $staff_name = $staff['staff_name'];
       } else {
         $staff_name = $_SESSION['user']['staff_name'];
@@ -162,6 +170,7 @@ class Classes extends Controller implements Actions
     if ($_SESSION['user']['role'] == 'student') {
       if (isset($_SESSION['profile_change'])) {
         $student = $this->model('Student_Model')->getStudentBySIN($_SESSION['user']['sin']);
+
         $student_name = $student['student_name'];
       } else {
         $student_name = $_SESSION['user']['student_name'];
@@ -174,20 +183,25 @@ class Classes extends Controller implements Actions
     // Pagination
     if (isset($_SESSION['search_class_keyword']) && $_SESSION['search_class_keyword'] != '') {
       $total_data = count($this->model('Class_Model')->getClassByAny($_SESSION['search_class_keyword']));
+
+      $data['class_count'] = $total_data;
     } else {
       $total_data = count($this->model('Class_Model')->getAllClasses());
+
+      $data['class_count'] = $total_data;
     }
-    $data['class_amount'] = $total_data;
 
 
     if (isset($_POST['search-class'])) {
-      $class = $this->model('Class_Model')->getClassByAny($_POST['class-field']);
-      $total_data = count($class);
-      $data['class_amount'] = $total_data;
-      $_SESSION['search_class_keyword'] = $_POST['class-field'];
+      $search_class_keyword = $_POST['search-class-keyword'];
+
+      $total_data = count($this->model('Class_Model')->getClassByAny($search_class_keyword));
+
+      $data['class_count'] = $total_data;
+      $_SESSION['search_class_keyword'] = $search_class_keyword;
 
       if ($total_data < 6) {
-        header('location: ' . BASEURL . 'classes/index');
+        header('location: ' . BASEURL . 'classes/page/1');
         exit;
       }
     }
@@ -202,7 +216,7 @@ class Classes extends Controller implements Actions
     $total_page = ceil($total_data / $total_data_per_page);
 
     if ($total_page <= 1 && $page != 1) {
-      header('Location: ' . BASEURL . 'classes/index');
+      header('Location: ' . BASEURL . 'classes/page/1');
       exit;
     }
 
@@ -244,6 +258,7 @@ class Classes extends Controller implements Actions
 
     for ($i = 0; $i < count($data['class']); $i++) {
       $students = $this->model('Student_Model')->getStudentsByClassId($data['class'][$i]['class_id']);
+
       $data['class'][$i]['total_students'] = count($students);
     }
 
@@ -256,9 +271,11 @@ class Classes extends Controller implements Actions
       'start_data' => $start_data,
       'end_data' => $end_data,
     ];
+
     $data['title'] = 'Propay - Class';
     $data['breadcrumb'] = 'Classes';
     $data['keyword'] = $_SESSION['search_class_keyword'] ?? '';
+
     $this->view('templates/header', $data, 'class/index');
     $this->view('templates/sidebar', $data, 'class/index');
     $this->view('templates/top-bar', $data, 'class' . $page . '/index');
@@ -269,33 +286,29 @@ class Classes extends Controller implements Actions
   public function update_action()
   {
     $json = file_get_contents('php://input');
+
     $data = json_decode($json, true);
+
     $result = $this->model("Class_Model")->updateClass($data);
 
     if ($result['row_count'] > 0) {
       $response = [
         'status' => 'success',
-        'message' => 'Class has been successfully update',
         'class_id' =>  $result['class_id'],
         'url' => BASEURL . 'classes/index'
       ];
-      file_put_contents('php://output', json_encode($response));
     } else if ($result['row_count'] == 0) {
       $response = [
         'status' => 'nothing-update',
-        'message' => 'No class data update',
         'class_id' =>  $result['class_id'],
         'url' => BASEURL . 'classes/index'
       ];
-      file_put_contents('php://output', json_encode($response));
     } else {
       $response = [
         'status' => 'error',
-        'message' => 'Failed to update class',
-        'url' => BASEURL . 'classes/index'
       ];
-      file_put_contents('php://output', json_encode($response));
     }
+    file_put_contents('php://output', json_encode($response));
   }
 
   public function check_action()
@@ -325,33 +338,45 @@ class Classes extends Controller implements Actions
   public function delete_action($class_id)
   {
     file_get_contents('php://input');
+
     $class  = $this->model("Class_Model")->getClassById($class_id);
+
     $class_row_count = $this->model("Class_Model")->deleteClass($class_id);
 
     if ($class_row_count) {
-      file_put_contents('php://output', json_encode([
-        'status_message' => 'success',
-        'status_code' => 200,
+      $response = [
+        'status' => 'success',
         'class_name' => $class['class_name']
-      ]));
+      ];
+    } else {
+      $response = [
+        'status' => 'success',
+      ];
     }
+
+    file_put_contents('php://output', json_encode($response));
   }
 
   public function insert_action()
   {
     $json  = file_get_contents('php://input');
+
     $data = json_decode($json, true);
+
     $class_row_count = $this->model("Class_Model")->addClass($data);
 
     if ($class_row_count) {
       $response = [
         'status' => 'success',
-        'message' => 'Class has been successfully added',
         'class_name' => $data['class_name'],
         'url' => BASEURL . 'classes'
       ];
-
-      file_put_contents('php://output', json_encode($response));
+    } else {
+      $response = [
+        'status' => 'error',
+      ];
     }
+
+    file_put_contents('php://output', json_encode($response));
   }
 }

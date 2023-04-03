7b8a902ae023ecc8c562129c98cc1bc52d9d083f
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-04-03 09:27:36 +0800

    Refactor code dashboard controller"

--- app/controllers/Dashboard.php
+++ app/controllers/Dashboard.php
@@ -15,61 +15,68 @@ class Dashboard extends Controller
     unset($_SESSION['last_search']);
     unset($_SESSION['row_per_page']);
 
-
     if ($_SESSION['user']['role'] == 'admin' || $_SESSION['user']['role'] == 'staff') {
       if (isset($_SESSION['profile_change'])) {
         $staff = $this->model('Staff_Model')->getStaffById($_SESSION['user']['staff_id']);
-        $staff_name = $staff['staff_name'];
+
+        $full_name = $staff['staff_name'];
       } else {
-        $staff_name = $_SESSION['user']['staff_name'];
+        $full_name = $_SESSION['user']['staff_name'];
       }
 
-      $secondAndThirdOfStaffName = explode(' ', $staff_name)[0] . ' ' . explode(' ', $staff_name)[1];
-      $data['greeting_name'] = $secondAndThirdOfStaffName;
-      $data['name'] = $staff_name;
+      $first_name = explode(' ', $full_name)[0];
+      $second_name = explode(' ', $full_name)[1];
+      $staff_name = "$first_name $second_name";
+
+      $data['greeting_name'] = $staff_name;
+      $data['name'] = $full_name;
       $data['role'] = $_SESSION['user']['staff_level'];
     }
 
     if ($_SESSION['user']['role'] == 'student') {
       if (isset($_SESSION['profile_change'])) {
         $student = $this->model('Student_Model')->getStudentBySIN($_SESSION['user']['sin']);
-        $student_name = $student['student_name'];
+
+        $full_name = $student['student_name'];
       } else {
-        $student_name = $_SESSION['user']['student_name'];
+        $full_name = $_SESSION['user']['student_name'];
       }
 
-      $secondAndThirdOfStaffName = explode(' ', $student_name)[0] . ' ' . explode(' ', $student_name)[1];
-      $data['greeting_name'] = $secondAndThirdOfStaffName;
-      $data['name'] = $student_name;
+      $first_name = explode(' ', $full_name)[0];
+      $second_name = explode(' ', $full_name)[1];
+      $student_name = "$first_name $second_name";
+
+      $data['greeting_name'] = $student_name;
+      $data['name'] = $full_name;
       $data['role'] = 'student';
     }
 
+    $payment_histories = $this->model('Payment_History_Model')->getPaymentHistoryWithLimit(0, 4,  null, null, true);
 
-    $payment_history = $this->model('Payment_History_Model')->getAllPaymentHistories(true);
+    for ($i = 0; $i < count($payment_histories); $i++) {
+      $payment_histories[$i]['payment'] = $this->model('Payment_Model')->getPaymentById($payment_histories[$i]['payment_id']);
 
-    for ($i = 0; $i < count($payment_history); $i++) {
-      $payment_history[$i]['payment'] = $this->model('Payment_Model')->getPaymentById($payment_history[$i]['payment_id']);
-      $payment_history[$i]['staff'] = $this->model('Staff_Model')->getStaffById($payment_history[$i]['staff_id']);
+      $payment_histories[$i]['staff'] = $this->model('Staff_Model')->getStaffById($payment_histories[$i]['staff_id']);
+
+      $payment_histories[$i]['student'] = $this->model('Student_Model')->getStudentBySIN($payment_histories[$i]['payment']['sin']);
     }
 
-    for ($i = 0; $i < count($payment_history); $i++) {
-      $payment_history[$i]['student'] = $this->model('Student_Model')->getStudentBySIN($payment_history[$i]['payment']['sin']);
-    }
-
-    $student_payment_history = [];
+    $student_payment_histories = array();
 
     if ($_SESSION['user']['role'] == 'student') {
-      for ($i = 0; $i < count($payment_history); $i++) {
-        if ($payment_history[$i]['payment']['sin'] == $_SESSION['user']['sin']) {
-          array_push($student_payment_history, $payment_history[$i]);
+      for ($i = 0; $i < count($payment_histories); $i++) {
+        if ($payment_histories[$i]['payment']['sin'] == $_SESSION['user']['sin']) {
+          array_push($student_payment_histories, $payment_histories[$i]);
         }
       }
     }
 
-    if (count($student_payment_history) > 0) {
-      $data['payment_history'] = $student_payment_history;
-    } elseif ($_SESSION['user']['role'] != 'student') {
-      $data['payment_history'] = $payment_history;
+    if (count($student_payment_histories) > 0) {
+      $data['payment_histories'] = $student_payment_histories;
+    }
+
+    if ($_SESSION['user']['role'] != 'student') {
+      $data['payment_histories'] = $payment_histories;
     }
 
     $data['title'] = 'Propay - Dashboard';
@@ -77,6 +84,7 @@ class Dashboard extends Controller
     $data['total_student'] = count($this->model('Student_model')->getAllStudents());
     $data['total_class'] = count($this->model('Class_model')->getAllClasses());
     $data['total_staff'] = count($this->model('Staff_Model')->getAllStaff());
+
     $this->view('templates/header', $data, 'dashboard');
     $this->view('templates/sidebar', $data, 'dashboard');
     $this->view('templates/top-bar', $data, 'dashboard');

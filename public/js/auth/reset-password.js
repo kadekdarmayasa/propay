19b330b9234041c21d955fddb34c99e128e2f24d
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-04-06 09:19:06 +0800

    add file reset-password.js for authentication validation

--- public/js/auth/reset-password.js
+++ public/js/auth/reset-password.js
@@ -4,21 +4,27 @@ const form = document.getElementById('reset_password');
 
 if (form) {
 	const saveChangesBtn = document.querySelector('.save-changes');
+
 	saveChangesBtn.style.visibility = 'hidden';
 
 	form.addEventListener('keyup', function (e) {
 		const isContainError = validateInputs(e.target, 'reset-password', this);
+
 		saveChangesBtn.style.visibility = isContainError ? 'hidden' : 'visible';
-		saveChangesBtn.style.visibility = isContainError ? 'hidden' : 'visible';
+		saveChangesBtn.disabled = isContainError ? true : false;
 	});
+
 	form.addEventListener('change', function (e) {
 		const isContainError = validateInputs(e.target, 'reset-password', this);
+
 		saveChangesBtn.style.visibility = isContainError ? 'hidden' : 'visible';
-		saveChangesBtn.style.visibility = isContainError ? 'hidden' : 'visible';
+		saveChangesBtn.disabled = isContainError ? true : false;
 	});
-	form.addEventListener('keydown', function (e) {
+
+	form.addEventListener('input', function (e) {
 		const isContainError = validateInputs(e.target, 'reset-password', this);
+
 		saveChangesBtn.style.visibility = isContainError ? 'hidden' : 'visible';
-		saveChangesBtn.style.visibility = isContainError ? 'hidden' : 'visible';
+		saveChangesBtn.disabled = isContainError ? true : false;
 	});
 }

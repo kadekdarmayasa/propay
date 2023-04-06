b7d2ed83b06382404df93a46b232ccb4fc87bccf
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-04-06 08:57:04 +0800

    window.addEventListener('load', function () { if (window.history.replaceState) { window.history.replaceState(null, null, window.location.href); } })

--- public/js/payment/form-overlay.js
+++ public/js/payment/form-overlay.js
@@ -1,12 +1,15 @@
 import '../components/overlay.js';
+import validateInputs from '../helpers/validator/index.js';
+import Payment from '../helpers/payment.js';
+import formatDate from '../utils/date_format.js';
 
+const paymentForm = document.querySelector('#payment-form');
 const closeBtn = document.querySelector('.close-btn');
 const overlayInputForm = document.querySelector('.overlay-input-form');
 const payButtons = document.querySelectorAll('.pay-btn');
-const paymentAmount = document.querySelector('#payment_amount');
 const paymentId = document.querySelector('#payment_id');
 const dateOfPayment = document.querySelector('#date_of_payment');
-const paymentForm = document.querySelector('#payment-form');
+const submitBtn = document.querySelector('.submit-btn');
 
 const overlay = document.querySelector('.overlay');
 const overlayCancelBtn = document.getElementById('overlay-cancel-btn');
@@ -15,38 +18,76 @@ const overlayMetaTitle = document.querySelector('.overlay .title');
 const overlayMetaDescription = document.querySelector('.overlay .description');
 const icon = document.querySelector('.icon');
 
-paymentForm.addEventListener('submit', async function (e) {
-	if (!parseInt(paymentAmount.value)) {
-		e.preventDefault();
-		this.reset();
+submitBtn.style.visibility = 'hidden';
+submitBtn.disabled = true;
+
+window.addEventListener('click', (e) => {
+	if (e.target === overlayInputForm) {
 		overlayInputForm.classList.remove('show');
 	}
+});
 
-	if (paymentAmount.value != '' || dateOfPayment.value != '') {
+payButtons.forEach((payBtn) => {
+	payBtn.addEventListener('click', function (e) {
+		paymentId.value = this.dataset.paymentId;
+		dateOfPayment.value = formatDate(this.dataset.paymentDate);
+
+		overlayInputForm.classList.add('show');
 		e.preventDefault();
+	});
+});
 
-		const inputs = [...this.querySelectorAll('.input')];
+paymentForm.addEventListener('input', function (e) {
+	const isContainError = validateInputs(e.target, 'payment', this);
 
-		const data = {};
-		inputs.forEach((input) => {
-			data[input.id] = input.value;
-		});
+	submitBtn.style.visibility = isContainError ? 'hidden' : 'visible';
+	submitBtn.disabled = isContainError ? true : false;
+});
 
-		const response = await payment(data);
+paymentForm.addEventListener('change', function (e) {
+	const isContainError = validateInputs(e.target, 'payment', this);
+
+	submitBtn.style.visibility = isContainError ? 'hidden' : 'visible';
+	submitBtn.disabled = isContainError ? true : false;
+});
+
+paymentForm.addEventListener('keyup', function (e) {
+	const isContainError = validateInputs(e.target, 'payment', this);
+
+	submitBtn.style.visibility = isContainError ? 'hidden' : 'visible';
+	submitBtn.disabled = isContainError ? true : false;
+});
+
+paymentForm.addEventListener('submit', async function (e) {
+	e.preventDefault();
+
+	const data = {};
+
+	const inputs = document.querySelectorAll('.input');
+
+	inputs.forEach((input) => {
+		data[input.id] = input.value;
+	});
+
+	try {
+		const payment = new Payment('http://localhost/propay/payment/payment_action');
+		const response = await payment.process(data);
 
 		if (response.status == 'success') {
 			this.reset();
 			overlayInputForm.classList.remove('show');
 
 			icon.innerHTML = /* html */ `
-				<svg width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<rect x="0.5" y="0.5" width="32" height="32" rx="16" stroke="black"/>
-					<path d="M9.5 15L15.5 22.5L24 10.5" stroke="black" stroke-linecap="round"/>
+				<svg width="52" height="52" viewBox="0 0 52 52" fill="none" xmlns="http://www.w3.org/2000/svg" class="success">
+					<rect x="2.5" y="2.5" width="47" height="47" rx="23.5" stroke="black"/>
+					<path d="M15 23.5L24.5 34L39.5 18" stroke="black" stroke-linecap="round"/>
 				</svg>
-`;
+			`;
+
 			overlayMetaTitle.textContent = response.message;
 
 			if (response.refund != 'Rp. 0') {
+				overlayMetaDescription.style.lineHeight = '1.8';
 				overlayMetaDescription.innerHTML = response.description + '<br>' + ' with Total Refund <b>' + response.refund + ' </b>';
 			} else {
 				overlayMetaDescription.innerHTML = response.description;
@@ -61,65 +102,34 @@ paymentForm.addEventListener('submit', async function (e) {
 				location.reload();
 			});
 		}
-	}
-});
-
-async function payment(payment_data) {
-	const data = payment_data;
-	const url = 'http://localhost/propay/edc_payment/payment_action';
-
-	const response = await fetch(url, {
-		method: 'POST',
-		mode: 'no-cors',
-		credentials: 'same-origin',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify(data),
-	});
-
-	return response.json();
-}
-
-window.addEventListener('click', (e) => {
-	if (e.target === overlayInputForm) {
+	} catch (e) {
+		this.reset();
 		overlayInputForm.classList.remove('show');
+
+		icon.innerHTML = /* html */ `
+			<svg width="52" height="52" viewBox="0 0 52 52" fill="none" xmlns="http://www.w3.org/2000/svg" class="error">
+					<rect x="2.5" y="2.5" width="47" height="47" rx="23.5" stroke="black"/>
+					<path d="M15 15.8505L37.5 36.3505" stroke="black" stroke-linecap="round"/>
+					<path d="M15.0585 36.4144L37.4415 15.7867" stroke="black" stroke-linecap="round"/>
+			</svg>
+		`;
+
+		overlayMetaTitle.textContent = 'Oops!!!';
+		overlayMetaDescription.innerHTML = 'Your payment failed to process';
+
+		overlay.classList.add('show');
+
+		overlayDeleteBtn.style.display = 'none';
+		overlayCancelBtn.textContent = 'Close';
+
+		overlayCancelBtn.addEventListener('click', () => {
+			location.reload();
+		});
 	}
+
 });
 
 closeBtn.addEventListener('click', () => {
 	overlayInputForm.classList.remove('show');
 });
 
-payButtons.forEach((payBtn) => {
-	payBtn.addEventListener('click', function (e) {
-		paymentId.value = this.dataset.paymentId;
-		dateOfPayment.value = formatDate(this.dataset.paymentDate);
-		overlayInputForm.classList.add('show');
-		e.preventDefault();
-	});
-});
-
-function formatDate(strDate) {
-	const months = {
-		Jan: '01',
-		Feb: '02',
-		Mar: '03',
-		Apr: '04',
-		May: '05',
-		Jun: '06',
-		Jul: '07',
-		Aug: '08',
-		Sep: '09',
-		Oct: '10',
-		Nov: '11',
-		Dec: '12',
-	};
-
-	const dates = strDate.split(' ');
-	const date = dates[0];
-	const month = months[dates[1]];
-	const year = dates[2];
-
-	return `${year}-${month}-${date}`;
-}

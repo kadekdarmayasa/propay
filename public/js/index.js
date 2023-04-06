1e6bfe3c58b464c767d6c77217d0c0d2bc9bd8b0
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-04-06 09:13:16 +0800

    Add history replaceState when window loaded

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,13 @@
 const body = document.querySelector('body');
 
 let getMode = localStorage.getItem('mode');
+
 if (getMode && getMode === 'dark') {
 	body.classList.add('dark');
 } else {
 	body.classList.remove('dark');
 }
+
+window.addEventListener('load', () => {
+	window.history.replaceState(null, null, window.location.href);
+});

ae73e35c0b616c2ee47859ca2c37602c2f95bba0
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-04-06 09:21:33 +0800

    Add hidden display to alert when there is no error occured

--- app/core/Flasher.php
+++ app/core/Flasher.php
@@ -20,7 +20,7 @@ class Flasher
       $alert_message = $_SESSION['flasher']['message'];
       $close_icon = BASEURL . 'public/images/close.svg';
 
-      echo " <div class='alert $alert_type'>
+      echo " <div class='alert $alert_type' style='display: flex !important;'>
         <div class='alert-left'>
           <img src='$illustration_icon' alt='error-icon'>
           <p class='message'>$alert_message</p>

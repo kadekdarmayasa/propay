b731ad870ef93d23de4a5adcc1cce4be39d14d94
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-04-06 11:59:18 +0800

    Update route url

--- app/controllers/EDC.php
+++ app/controllers/EDC.php
@@ -5,14 +5,14 @@ class EDC extends Controller
   public function index()
   {
     unset($_SESSION['last_search']);
-    header('Location: ' . BASEURL . 'edc_List/index');
+    header('Location: ' . BASEURL . 'edc_list/index');
     exit;
   }
 
 
   public function payment()
   {
-    header('Location: ' . BASEURL . 'edc_payment/index');
+    header('Location: ' . BASEURL . 'payment/index');
     exit;
   }
 
@@ -20,14 +20,14 @@ class EDC extends Controller
   public function payment_history()
   {
     unset($_SESSION['last_search']);
-    header('Location: ' . BASEURL . 'edc_payment_history/index');
+    header('Location: ' . BASEURL . 'payment_history/index');
     exit;
   }
 
   public function report()
   {
     unset($_SESSION['last_search']);
-    header('Location: ' . BASEURL . 'edc_report/index');
+    header('Location: ' . BASEURL . 'payment_report/index');
     exit;
   }
 }
